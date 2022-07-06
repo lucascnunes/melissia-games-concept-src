03bc3cf81bd9b3be29adba3d1a27f6d166c41712
Lucas Nunes <lucas.parreira.nunes@gmail.com>
2022-07-06 09:31:26 -0300

    setting dependencies and importing them

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,131 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <loading-component />
+    <navigation-component />
+    <router-view />
+    <footer>
+      © 2020
+      <strong>MELISSIA GAMES</strong>
+      <!-- <br />
+      {{
+      $t("THIS SITE IS IN NO WAY ASSOCIATED WITH OR ENDORSED BY {name}", {
+      name: "PEARL ABYSS",
+      })
+      }} -->
+      ©
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoadingComponent from "./components/LoadingComponent";
+import NavigationComponent from "./components/NavigationComponent";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    LoadingComponent,
+    NavigationComponent
+  },
+  mounted() {
+    document.documentElement.setAttribute("data-theme", this.theme);
+  },
 }
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Marcellus&family=Open+Sans:wght@300;400;600;700&display=swap");
+:root {
+  --symbol-invert: invert(1);
+  --symbol-inverted: invert(0);
+  --font-color: #fff;
+  --bg-color: #232323;
+  --footer-bg-color: #fff;
+  --footer-font-color: #232323;
+  --sidebar-bg-color: #fff;
+  --sidebar-font-color: #232323;
+  --nav-item-background-color: transparent;
+  --btn-background-color: #fff;
+  --btn-font-color: #fff;
+  --btn-bg-color: #232323;
+  --btn-border-color: #232323;
+}
+
+[data-theme="dark"] {
+  --symbol-invert: invert(0);
+  --symbol-inverted: invert(1);
+  --font-color: #232323;
+  --bg-color: #fff;
+  --footer-bg-color: #232323;
+  --footer-font-color: #fff;
+  --sidebar-bg-color: #232323;
+  --sidebar-font-color: #fff;
+  --nav-item-background-color: transparent;
+  --btn-background-color: #232323;
+  --btn-font-color: #232323;
+  --btn-bg-color: #fff;
+  --btn-border-color: #fff;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Marcellus", serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.wrapper {
+  background: var(--bg-color);
+  color: var(--font-color);
+}
+footer {
+  background: var(--footer-bg-color);
+  color: var(--footer-font-color);
+  padding: 30px;
+  font-size: 15px;
+}
+.symbol {
+  width: 30px;
+  height: 30px;
+  filter: var(--symbol-invert);
+}
+.symbol-sidebar {
+  filter: var(--symbol-inverted) !important;
+}
+.py-10 {
+  padding: 10em 0;
+}
+.navbar-brand {
+  letter-spacing: 4px;
+  text-transform: uppercase;
+  font-size: 12px !important;
+}
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Opera and Firefox */
+}
+.bg-232323 {
+  background-color: #232323 !important;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(0.95);
+  }
+
+  70% {
+    transform: scale(1.1);
+  }
+
+  100% {
+    transform: scale(0.95);
+  }
+}
+
+li.nav-item:not(.active),
+li.nav-item.active {
+  background: transparent !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,56 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+import { createPinia } from 'pinia'
+import "bootstrap/dist/css/bootstrap.css";
+import router from "./routes";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { 
+    faUserPlus,
+    faUser,
+    faBars,
+    faTimes,
+    faSun,
+    faMoon,
+    faClipboard,
+    faKey,
+    faAt,
+    faComment,
+    faInfo,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faDiscord,
+  faTelegram
+} from "@fortawesome/free-brands-svg-icons";
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+library.add(
+    faUserPlus,
+    faUser,
+    faDiscord,
+    faTelegram,
+    faBars,
+    faTimes,
+    faSun,
+    faMoon,
+    faClipboard,
+    faKey,
+    faComment,
+    faInfo,
+    faAt
+  );
+
+const pinia = createPinia()
+const i18n = createI18n()
+  
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+app.use(pinia)
+app.use(i18n)
+
+app.mount('#app')
